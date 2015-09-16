f0258cd6d06c459b4bb9b5fe2c8436163256d5d2
Sabeur Thabti <thabti.saber@gmailcom>
2015-09-16 02:27:39 +0100

    removing node-sass, code write.

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
-var css = require('../build/index.js');
-
-css.parse('./test/style.css', './test/style-css.js');
-css.parse('./test/style.scss', './test/style-scss.js');
-
-
+//var css = require('../build/index.js');
+//
+//css.parse('./test/style.css', './test/style-css.js');
+//css.parse('./test/style.scss', './test/style-scss.js');
+//
+//
