90e866636f16691a1478ab2734c63e50908c289a
Sabeur Thabti <thabti.sabeur@gmail.com>
2016-03-12 10:29:40 +0000

    bump

---
+++ test/fixtures/stylescss-dorthwein.js
@@ -0,0 +1,1 @@
+module.exports = require('react-native').StyleSheet.create({"text":{"color":"#000"},"center":{"textAlign":"center","alignItems":"center","alignSelf":"center","justifyContent":"center"}});
